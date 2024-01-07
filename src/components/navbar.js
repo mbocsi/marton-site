c36f739d58205e91ec47d6bd8b37bfd725033a72
Marton Bocsi <mbocsi@wisc.edu>
2024-01-06 19:52:43 -0600

    Social links work

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,11 +1,12 @@
 import './navbar.css';
-import { Button, Icon, Box } from '@chakra-ui/react';
+import { Button, Icon, Box, useClipboard } from '@chakra-ui/react';
 import { EmailIcon } from '@chakra-ui/icons';
-import { FaFacebook, FaInstagram } from 'react-icons/fa';
+import { FaFacebook, FaInstagram, FaGithub } from 'react-icons/fa';
 import { motion } from 'framer-motion';
 import { Link } from 'react-router-dom';
 
 function Navbar() {
+    const { onCopy, value, setValue, hasCopied } = useClipboard("mvbocsi@gmail.com");
     return (
         <div className='navbar'>
             <Link className='navitem' to='/'>
@@ -22,13 +23,22 @@ function Navbar() {
             </Link>
             
             <Button className='socialitem' colorScheme='instagram' as={motion.div} whileHover={{ scale: 1.2}} whileTap={{ scale: 1.1 }}>
-                <Icon boxSize={8} as={FaFacebook}></Icon>
+                <Link to='https://www.facebook.com/marton.bocsi'>
+                    <Icon boxSize={8} as={FaFacebook}></Icon>
+                </Link>
             </Button>
             <Button colorScheme='instagram' as={motion.div} whileHover={{ scale: 1.2}} whileTap={{ scale: 1.1 }}>
-                <Icon boxSize={8} as={FaInstagram}></Icon>
+                <Link to='https://www.instagram.com/martonbocsi/'>
+                    <Icon boxSize={8} as={FaInstagram}></Icon>
+                </Link>
             </Button>
-            <Button colorScheme='instagram' as={motion.div} whileHover={{ scale: 1.2}} whileTap={{ scale: 1.1 }}>
+            <Button onClick={onCopy} colorScheme='instagram' as={motion.div} whileHover={{ scale: 1.2}} whileTap={{ scale: 1.1 }}>
                 <Icon boxSize={8} as={EmailIcon}></Icon>
+            </Button>
+            <Button colorScheme='instagram' as={motion.div} whileHover={{ scale: 1.2}} whileTap={{ scale: 1.1 }}>
+                <Link to='https://github.com/mbocsi'>
+                    <Icon boxSize={8} as={FaGithub}></Icon>
+                </Link>
             </Button>
         </div>
     )
